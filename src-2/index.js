ccef8ea5956b9cc9c0269aca18fe4314e71320ef
Olek Myron <myrondell@gmail.com>
2021-07-24 15:25:15 +0300

    refact

--- src-2/index.js
+++
@@ -1,6 +0,0 @@
-import './sass/splide.min.css';
-import './sass/main.scss';
-import './js/image-finder';
-import 'material-design-icons';
-import './js/splide';
-import './js/bLightbox';
--- src-2/js/bLightbox.js
+++
@@ -1,5 +0,0 @@
-// const basicLightbox = require('basiclightbox');
-console.log('dfgdfgdf');
-// import * as basicLightbox from 'basiclightbox';
-
-console.log(basicLightbox);
